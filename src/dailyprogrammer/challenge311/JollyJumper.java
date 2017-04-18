37cdf612f5b62f69a0a43182e7fbc09170629e20
Michael Bespalov <mich8bsp@gmail.com>
2017-04-18 23:09:23 +0300

    Challenge #311

---
+++ src/dailyprogrammer/challenge311/JollyJumper.java
@@ -0,0 +1,40 @@
+package dailyprogrammer.challenge311;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+/**
+ * Created by mich8 on 18-Apr-17.
+ */
+public class JollyJumper {
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        JollyJumper jumper = new JollyJumper();
+        Files.readAllLines(Paths.get(JollyJumper.class.getResource(".\\input.txt").toURI())).stream()
+                .map(jumper::isJolly)
+                .map(isJolly -> isJolly ? "JOLLY" : "NOT JOLLY")
+                .forEachOrdered(System.out::println);
+    }
+
+    private boolean isJolly(String line) {
+        int[] inputLine = Arrays.stream(line.split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int n = inputLine[0];
+
+        Map<Integer, Boolean> numToIsPresent = new HashMap<>(n - 1);
+        IntStream.range(1, n).forEach(i -> numToIsPresent.put(i, false));
+        IntStream.range(1, inputLine.length-1).forEach(i -> {
+            int absDifference = Math.abs(inputLine[i + 1] - inputLine[i]);
+            numToIsPresent.put(absDifference, true);
+        });
+        return numToIsPresent.values().stream().reduce(true, (x,y)->x&&y);
+    }
+}
